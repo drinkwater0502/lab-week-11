58a4690ecbc1f010e6f2cd5e4509bbb8e23e08c9
Simon Jung <drinkwater0502@gmail.com>
2021-03-25 20:14:19 -0700

    fixed most of JS

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,19 @@
-// your JavaScript goes here, you can delete this comment
+const todoList = [
+    {todo: "take dog on walk", status: "incomplete"},
+    {todo: "wash the dishes", status: "complete"},
+    {todo: "eat cake", status: "incomplete"},
+    {todo: "drink water", status: "complete"}
+]
+let completedTodos = []
+
+for (task of todoList) {
+    if (task.status == 'complete') {
+        completedTodos.push(task) 
+    }
+}
+
+for ({todo} of completedTodos) {
+    console.log(todo)
+}
+
+
